4547f7f2063cfff91525f04c50179a39fae3c279
Marcos Couto <marcos.couto@zup.com.br>
2020-11-11 22:33:32 -0300

    Primeiro commit

---
+++ src/main/kotlin/com/marcoscoutozup/ecommercekt/EcommercektApplication.kt
@@ -0,0 +1,11 @@
+package com.marcoscoutozup.ecommercekt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcommercektApplication
+
+fun main(args: Array<String>) {
+	runApplication<EcommercektApplication>(*args)
+}
